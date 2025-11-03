c0095cfcf8dd2e76423cf75df0050655070aeb63
brandonstanaway <bstanaway.econ@gmail.com>
2025-11-03 16:45:24 -0500

    Working on the multivariate bar graph function

--- urbansim_model_update/projections_functions.R
+++ urbansim_model_update/projections_functions.R
@@ -60,7 +60,41 @@ sv.bar.chart_ <- function(df, x_var, y_var, x_label, y_label, custom_title, geo)
 
 # Multi-Variable Stacked Bar Charts
 
-mv.bar.chart_ <- function(){
+mv.bar.chart_ <- function(df, x_var, y_var, group_var, x_label, y_label, group_label, custom_title, geo){
+  
+  graph <- ggplot(data = get(df), aes(fill = get(group_var), x = get(x_var), y = get(y_var))) +
+    geom_col(
+      position = "stack",
+      stat = "identity",
+      width = .5,
+      # fill = group_var,
+      color = "black"
+    ) +
+    geom_text(aes(label = scales::comma(get(y_var))), vjust = -1, position = position_dodge(.9)) +
+    labs(
+      title = paste0(custom_title, ", ", geo ," (2020-2050)"),
+      x = x_label,
+      y = y_label,
+      fill = group_label,
+      caption = "Source: MAPC Data Services - Research Division"
+    ) +
+    scale_y_continuous(labels = scales::label_comma(), expand = expansion(mult = c(0, .1))) +
+    theme_bw() +
+    theme(
+      panel.background = element_blank(),
+      panel.border = element_rect(
+        color = "black",
+        fill = NA,
+        size = 1
+      ),
+      plot.title = element_text(color = "black", size = 18),
+      axis.title.x = element_text(color = "black", size = 16),
+      axis.title.y = element_text(color = "black", size = 16),
+      axis.text.x = element_text(color = "black", size = 14),
+      axis.text.y = element_text(color = "black", size = 14)
+    )
+  
+  return(graph)
   
 }
 
