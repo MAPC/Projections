1ef83a5d2124acdb7993e6ef452d57f87be336c7
brandonstanaway <bstanaway.econ@gmail.com>
2025-11-14 11:02:37 -0500

    Mediocre single variable line charts are live

--- urbansim_model_update/projections_functions.R
+++ urbansim_model_update/projections_functions.R
@@ -101,7 +101,35 @@ mv.bar.chart_ <- function(df, x_var, y_var, group_var, x_label, y_label, group_l
 
 # Single Variable Line Charts
 
-sv.line.chart <- function(){
+sv.line.chart <- function(df, x_var, y_var, x_label, y_label, custom_title, geo){
+  
+  graph <- ggplot(data = get(df), aes(x = get(x_var), y = get(y_var), group = 1)) +
+    geom_line() +
+    geom_point() +
+    geom_text(aes(label = scales::comma(get(y_var))), vjust = -1) +
+    labs(
+      title = paste0(custom_title, ", ", geo ," (2020-2050)"),
+      x = x_label,
+      y = y_label,
+      caption = "Source: MAPC Data Services - Research Division"
+    ) +
+    scale_y_continuous(labels = scales::label_comma(), expand = expansion(mult = c(0, .1))) +
+    theme_bw() +
+    theme(
+      panel.background = element_blank(),
+      panel.border = element_rect(
+        color = "black",
+        fill = NA,
+        size = 1
+      ),
+      plot.title = element_text(color = "black", size = 18),
+      axis.title.x = element_text(color = "black", size = 16),
+      axis.title.y = element_text(color = "black", size = 16),
+      axis.text.x = element_text(color = "black", size = 14),
+      axis.text.y = element_text(color = "black", size = 14)
+    )
+  
+  return(graph)
   
 }
 
