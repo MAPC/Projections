1a451fa0a30638574ab4c713881eeddd0e47f21b
cgately <3432862+cgately@users.noreply.github.com>
2026-02-05 06:53:20 -0500

    warren merge

--- scripts/warren_group_processing.R
+++ scripts/warren_group_processing.R
@@ -8,7 +8,7 @@ lun <- function(x){length(unique(x))}
 
 wr <- fread('20250703_warren_group_2000_2024_residential_final.csv')
 
-to_geocode <- wr[lon==0]
+to_geocode <- wr[lat<1]
 to_geocode[,alt_add:=gsub(" #"," Unit ",geo_addr)]
 
 results1 <- geo_combine(
@@ -52,10 +52,27 @@ results <- rbind(results1,results2,results3)
 fwrite(results,'geocoded_NAs_20260205.csv')
 
 pass_two <- results[is.na(lat),address]
-    
 if(dim(pass_two)[1]<10000 & dim(pass_two)[1]>0){
-results2 <- pass_two |> 
+results4 <- pass_two |> 
   geo(method = 'google', lat = latitude , lon = longitude) %>% 
   setDT()
 }
+
+if(exists('results4')){results <- rbind(results,results4)}
+
+setnames(results,c('geo_addr','lat_gc','lon_gc','gc_method'))
+setkey(results, geo_addr)
+setkey(wr, geo_addr)
+
+wr.gc <- wr |>
+  left_join(results, by = 'geo_addr') |>
+  mutate(
+    lat = case_when(lat == 0 ~ lat.gc, .default = lat),
+    lon = case_when(lon == 0 ~ lon.gc, .default = lon)
+  ) |> 
+  select(-lat.gc,-lon.gc) |> 
+  relocate(gc_method,.after = ct20_id)
+
+wr.pts <- st_as_sf(wr.gc, coords=c('lon','lat'))
+
 
