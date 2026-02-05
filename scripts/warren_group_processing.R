2f7d58280eeb6e2a6de2a84f4062ba945f2316a3
cgately <3432862+cgately@users.noreply.github.com>
2026-02-05 06:38:06 -0500

    warren group geocoding

--- scripts/warren_group_processing.R
+++ scripts/warren_group_processing.R
@@ -2,5 +2,60 @@ setwd('S:/Network Shares/K Drive/DataServices/Datasets/Housing/Warren Group - Ho
 library(tidyverse)
 library(data.table)
 library(mapcdatakeys)
+library(tidygeocoder)
+sun <- function(x){sort(unique(x))}
+lun <- function(x){length(unique(x))}
 
 wr <- fread('20250703_warren_group_2000_2024_residential_final.csv')
+
+to_geocode <- wr[lon==0]
+to_geocode[,alt_add:=gsub(" #"," Unit ",geo_addr)]
+
+results1 <- geo_combine(
+  queries = list(
+    list(method = 'census'), 
+    list(method = 'osm'),
+    list(method = 'arcgis')),
+  address = to_geocode$geo_addr[1:10000],
+  global_params = list(address = 'address'), 
+  lat = lat,
+  lon = lon
+) |> 
+  setDT()
+
+results2 <- geo_combine(
+  queries = list(
+    list(method = 'census'), 
+    list(method = 'osm'),
+    list(method = 'arcgis')),
+  address = to_geocode$geo_addr[10001:20000],
+  global_params = list(address = 'address'), 
+  lat = lat,
+  lon = lon
+) |> 
+  setDT()
+
+results3 <- geo_combine(
+  queries = list(
+    list(method = 'census'), 
+    list(method = 'osm'),
+    list(method = 'arcgis')),
+  address = to_geocode$geo_addr[20001:dim(to_geocode)[1]],
+  global_params = list(address = 'address'), 
+  lat = lat,
+  lon = lon
+) |> 
+  setDT()
+
+results <- rbind(results1,results2,results3)
+
+fwrite(results,'geocoded_NAs_20260205.csv')
+
+pass_two <- results[is.na(lat),address]
+    
+if(dim(pass_two)[1]<10000 & dim(pass_two)[1]>0){
+results2 <- pass_two |> 
+  geo(method = 'google', lat = latitude , lon = longitude) %>% 
+  setDT()
+}
+
