773452f1438782049bba95d7e0b4174137316d76
brandonstanaway <bstanaway.econ@gmail.com>
2026-09-21 12:15:22 -0400

    LRTP23 SHoP Comparison Script

---
+++ LRTP_2027/Scripts/projections_libraries.R
@@ -0,0 +1,25 @@
+################################################################################
+# Libraries for use in MAPC's Socioeconomic Projections Workflows
+# Author: Brandon Stanaway 
+# Contact: bstanaway@mapc.org, bstanaway.econ@gmail.com
+# Github: 
+# Date Created: 09/21/2026
+# 
+# Purpose: Create single file to be used for several different 
+# Socioeconomic Projections scripts
+#
+################################################################################
+
+library(tidyverse)
+library(tidycensus)
+library(lubridate)
+library(stringr)
+library(janitor)
+library(sf)
+library(mapcdatakeys)
+library(dplyr)
+library(readxl)
+library(collapse)
+library(tidytext)
+library(writexl)
+library(here)
